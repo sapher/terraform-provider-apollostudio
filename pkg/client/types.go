f1bcd06cba11ea0fb06b3b525b7fcad06db41b7a
Xaser <sapher@users.noreply.github.com>
2024-01-10 22:14:35 +0100

    feat: augment error message composition check (#23)
    Add more meaningful error messages to the subgraph resource.
    
    Co-authored-by: Anonymous <user@anonymous.com>

--- pkg/client/types.go
+++ pkg/client/types.go
@@ -31,54 +31,162 @@ type ChangeSummary struct {
 	Type  FieldChangeSummaryCounts `json:"type"`
 }
 
-type OperationsCheckResult struct {
-	Id                         string        `json:"id"`
-	ChangeSummary              ChangeSummary `json:"changeSummary"`
-	NumberOfAffectedOperations int           `json:"numberOfAffectedOperations"`
-	NumberOfCheckedOperations  int           `json:"numberOfCheckedOperations"`
+type AffectedQuery struct {
+	AlreadyApproved bool   `json:"alreadyApproved"`
+	AlreadyIgnored  bool   `json:"alreadyIgnored"`
+	DisplayName     string `json:"displayName"`
+	Id              string `json:"id"`
+	IsValid         bool   `json:"isValid"`
+	Name            string `json:"name"`
+	Signature       string `json:"signature"`
 }
 
-type OperationsCheckTask struct {
-	Id     string                  `json:"id"`
-	Result OperationsCheckResult   `json:"result"`
-	Status CheckWorkflowTaskStatus `json:"status"`
+type Change struct {
+	Code        string         `json:"code"`
+	Description string         `json:"description"`
+	Severity    ChangeSeverity `json:"severity"`
+	Category    ChangeCategory `json:"category"`
+}
+
+type OperationsCheckResult struct {
+	Id                         string          `json:"id"`
+	AffectedQueries            []AffectedQuery `json:"affectedQueries"`
+	Changes                    []Change        `json:"changes"`
+	ChangeSummary              ChangeSummary   `json:"changeSummary"`
+	NumberOfAffectedOperations int             `json:"numberOfAffectedOperations"`
+	NumberOfCheckedOperations  int             `json:"numberOfCheckedOperations"`
+}
+
+type SourceLocation struct {
+	Column int `json:"column"`
+	Line   int `json:"line"`
 }
 
 type SchemaCompositionError struct {
-	Code    string `json:"code"`
-	Message string `json:"message"`
+	Code      string           `json:"code"`
+	Message   string           `json:"message"`
+	Locations []SourceLocation `json:"locations"`
 }
 
 type CompositionResult struct {
 	Errors []SchemaCompositionError `json:"errors"`
 }
 
-type CompositionCheckTask struct {
-	Id                 string                  `json:"id"`
-	Result             CompositionResult       `json:"result"`
-	Status             CheckWorkflowTaskStatus `json:"status"`
-	CoreSchemaModified bool                    `json:"coreSchemaModified"`
+type Coordinate struct {
+	ByteOffset int `json:"byteOffset"`
+	Column     int `json:"column"`
+	Line       int `json:"line"`
+}
+
+type Location struct {
+	End          Coordinate `json:"end"`
+	Start        Coordinate `json:"start"`
+	SubgraphName string     `json:"subgraphName"`
 }
 
 type LintDiagnostic struct {
-	Coordinate string `json:"coordinate"`
-	Message    string `json:"message"`
+	Coordinate      string              `json:"coordinate"`
+	Message         string              `json:"message"`
+	Level           LintDiagnosticLevel `json:"level"`
+	Rule            string              `json:"rule"`
+	SourceLocations []Location          `json:"sourceLocations"`
+}
+
+type LintStats struct {
+	ErrorsCount   int `json:"errorsCount"`
+	IgnoredCount  int `json:"ignoredCount"`
+	TotalCount    int `json:"totalCount"`
+	WarningsCount int `json:"warningsCount"`
 }
 
 type LintResult struct {
 	Diagnostics []LintDiagnostic `json:"diagnostics"`
+	Stats       LintStats        `json:"stats"`
 }
 
-type LintCheckTask struct {
-	Id     string `json:"id"`
-	Result LintResult
+type DownstreamCheckResult struct {
+	Blocking              bool   `json:"blocking"`
+	DownstreamVariantName string `json:"downsteamVariantName"`
+	FailsUpstreamWorkflow bool   `json:"failsUpstreamWorkflow"`
 }
 
 const (
-	StatusFailed  CheckWorkflowTaskStatus = "FAILED"
-	StatusPassed  CheckWorkflowTaskStatus = "PASSED"
-	StatusBlocked CheckWorkflowTaskStatus = "BLOCKED"
-	StatusPending CheckWorkflowTaskStatus = "PENDING"
+	SeverityFailure ChangeSeverity = "FAILURE"
+	SeverityNotice  ChangeSeverity = "NOTICE"
+)
+
+type ChangeSeverity string
+
+const (
+	CheckWorkflowStatusFailed  CheckWorkflowStatus = "FAILED"
+	CheckWorkflowStatusPassed  CheckWorkflowStatus = "PASSED"
+	CheckWorkflowStatusPending CheckWorkflowStatus = "PENDING"
+)
+
+type CheckWorkflowStatus string
+
+const (
+	CheckWorkflowTaskStatusFailed  CheckWorkflowTaskStatus = "FAILED"
+	CheckWorkflowTaskStatusPassed  CheckWorkflowTaskStatus = "PASSED"
+	CheckWorkflowTaskStatusBlocked CheckWorkflowTaskStatus = "BLOCKED"
+	CheckWorkflowTaskStatusPending CheckWorkflowTaskStatus = "PENDING"
 )
 
 type CheckWorkflowTaskStatus string
+
+const (
+	DiagnosticLevelError   LintDiagnosticLevel = "ERROR"
+	DiagnosticLevelWarning LintDiagnosticLevel = "WARNING"
+	DiagnosticLevelIgnored LintDiagnosticLevel = "IGNORED"
+)
+
+type LintDiagnosticLevel string
+
+const (
+	CategoryAddition    ChangeCategory = "ADDITION"
+	CatergoryRemoval    ChangeCategory = "REMOVAL"
+	CategoryEdit        ChangeCategory = "EDIT"
+	CategoryDeprecation ChangeCategory = "DEPRECATION"
+)
+
+type ChangeCategory string
+
+const (
+	TaskTypeOperationsCheck  TaskTypename = "OperationsCheckTask"
+	TaskTypeCompositionCheck TaskTypename = "CompositionCheckTask"
+	TaskTypeLintCheck        TaskTypename = "LintCheckTask"
+	TaskTypeDownstreamCheck  TaskTypename = "DownstreamCheckTask"
+	TaskTypeProposalsCheck   TaskTypename = "ProposalsCheckTask"
+	TaskTypeFilterCheck      TaskTypename = "FilterCheckTask"
+)
+
+type TaskTypename string
+
+type OperationsCheckTask struct {
+	// Id string `json:"id"`
+	Result OperationsCheckResult   `json:"result"`
+	Status CheckWorkflowTaskStatus `json:"status"`
+}
+
+type CompositionCheckTask struct {
+	Result CompositionResult       `json:"result"`
+	Status CheckWorkflowTaskStatus `json:"status"`
+}
+
+type LintCheckTask struct {
+	Result LintResult
+	Status CheckWorkflowTaskStatus `json:"status"`
+}
+
+type DownstreamCheckTask struct {
+	Status  CheckWorkflowTaskStatus `json:"status"`
+	Results []DownstreamCheckResult `json:"results"`
+}
+
+type ProposalsCheckTask struct {
+	Status CheckWorkflowTaskStatus `json:"status"`
+}
+
+type FilterCheckTask struct {
+	Status CheckWorkflowTaskStatus `json:"status"`
+}
